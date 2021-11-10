02d1cb0a8bcd4bd73a544a915b312aaab733ab5c
sohel.rana <me.sohelmollah@gmail.com>
2021-11-10 17:34:27 +0600

    update

--- app/src/main/java/com/practice/calendarevent/data/model/Event.kt
+++ app/src/main/java/com/practice/calendarevent/data/model/Event.kt
@@ -7,6 +7,16 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "event_table")
 data class Event(
     @PrimaryKey(autoGenerate = true) val  id: Int,
+    @ColumnInfo(name = "status") val status: String,
     @ColumnInfo(name = "title") val title: String,
-    @ColumnInfo(name = "description") val description: String
-)
+    @ColumnInfo(name = "description") val description: String,
+    @ColumnInfo(name = "timeStamp") val timeStamp: String
+) {
+    constructor() : this(
+        id = 0,
+        status = "",
+        title = "",
+        description = "",
+        timeStamp = ""
+    )
+}
--- app/src/main/java/com/practice/calendarevent/view/MainActivity.kt
+++ app/src/main/java/com/practice/calendarevent/view/MainActivity.kt
@@ -1,54 +1,57 @@
 package com.practice.calendarevent.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
-import android.widget.CalendarView
-import android.widget.CalendarView.OnDateChangeListener
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.practice.calendarevent.R
 import com.practice.calendarevent.data.model.Event
 import com.practice.calendarevent.databinding.ActivityMainBinding
+import java.text.DateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var  customView: View
+    private lateinit var materialAlertDialogBuilder: MaterialAlertDialogBuilder
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
-        /*binding.calendarView.setOnDateChangeListener(object: OnDateChangeListener{
-            override fun onSelectedDayChange(p0: CalendarView, p1: Int, p2: Int, p3: Int) {
-                createDialog()
-                Toast.makeText(this@MainActivity, "Today: $p1/${p2+1}/$p3", Toast.LENGTH_SHORT).show()
-            }
-        })*/
-
+        materialAlertDialogBuilder = MaterialAlertDialogBuilder(this)
         binding.fab.setOnClickListener {
+            customView = LayoutInflater.from(this)
+                .inflate(R.layout.custom_layout, null, false)
             createDialog()
         }
     }
     private fun createDialog() {
-        val alertDialog = AlertDialog.Builder(this)
+        val title: TextView = customView.findViewById(R.id.title)
+        val description: TextView = customView.findViewById(R.id.description)
 
-        alertDialog.apply {
-            //setTitle("Create Event")
-            setMessage("Create new event?")
-            alertDialog.setView(layoutInflater.inflate(R.layout.custom_layout, null))
-            setPositiveButton("Yes") { _, _ ->
-                val positiveButtonStatus = "yes"
-                val title: TextView = findViewById(R.id.title)
-                val description: TextView = findViewById(R.id.description)
-                val titleText = title.text
-                val descriptionText = description.text
-                Log.d("TAG", "createDialog: $positiveButtonStatus, $titleText, ${title.id}, $descriptionText, ${description.id} ")
-            }
-            setNegativeButton("No") {_, _ ->
-                val negativeButtonStatus = "No"
-            }
-        }.create().show()
+        materialAlertDialogBuilder.setView(customView)
+            .setMessage("Create new event?")
+            .setTitle("Details")
+            .setMessage("Enter your basic details")
+            .setPositiveButton("Yes") { dialog, _ ->
+                val positiveButtonStatus = "Accept"
+                val titleText = title.text.toString()
+                val descriptionText = description.text.toString()
+                val timeStamp: String = DateFormat.getDateTimeInstance().format(Date())
+                Log.d("Sohel", "createDialog: $positiveButtonStatus, $titleText, $descriptionText $timeStamp ")
 
+            }
+            .setNegativeButton("No") { dialog, _ ->
+                val negativeButtonStatus = "Decline"
+                val titleText = title.text.toString()
+                val descriptionText = description.text.toString()
+                val timeStamp: String = DateFormat.getDateTimeInstance().format(Date())
+                Log.d("Sohel", "createDialog: $negativeButtonStatus, $titleText, $descriptionText $timeStamp ")
+            }.show()
     }
+
 }
