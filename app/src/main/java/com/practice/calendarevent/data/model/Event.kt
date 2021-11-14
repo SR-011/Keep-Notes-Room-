e355780a0a520b600c0b0da8cea469a2189486da
sohel.rana <me.sohelmollah@gmail.com>
2021-11-14 17:49:01 +0600

    parcelable

--- app/src/main/java/com/practice/calendarevent/data/model/Event.kt
+++ app/src/main/java/com/practice/calendarevent/data/model/Event.kt
@@ -1,9 +1,12 @@
 package com.practice.calendarevent.data.model
 
+import android.os.Parcelable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 @Entity(tableName = "event_table")
 data class Event(
     @PrimaryKey(autoGenerate = true) val  id: Int = 0,
@@ -11,4 +14,4 @@ data class Event(
     @ColumnInfo(name = "title") val title: String,
     @ColumnInfo(name = "description") val description: String,
     @ColumnInfo(name = "timeStamp") val timeStamp: String
-)
+): Parcelable
