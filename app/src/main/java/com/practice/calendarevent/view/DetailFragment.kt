30485a46ea4346233087dda730d8e98ca6f6a0b0
sohel.rana <me.sohelmollah@gmail.com>
2021-11-15 16:58:05 +0600

    activityViewModel

--- app/src/main/java/com/practice/calendarevent/view/DetailFragment.kt
+++ app/src/main/java/com/practice/calendarevent/view/DetailFragment.kt
@@ -2,13 +2,12 @@ package com.practice.calendarevent.view
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
 import com.practice.calendarevent.R
 import com.practice.calendarevent.data.model.Event
 import com.practice.calendarevent.databinding.FragmentDetailBinding
